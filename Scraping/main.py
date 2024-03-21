adb5a69ae951ba3509c1bcd0cc4c2c82ab52d301
Maria Petrova <m.petrova2@g.nsu.ru>
2024-03-21 18:43:13 +0700

    download images of 10 of the prettiest villages in the Lake District, Cumbria

--- Scraping/main.py
+++ Scraping/main.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 import sys
 
 storage_path = 'D:\\downloadedImages'
-url = 'https://www.wikipedia.org/'
-image_urls = []
+url = 'https://www.picturesofengland.com/England/Cumbria/article/1583'
+homeUrl = 'https://www.picturesofengland.com'
+
 data = ''
 try:
     response = requests.get(url)
@@ -19,16 +20,18 @@ soup = BeautifulSoup(data, 'html.parser')
 images = soup.find_all('img')
 
 for img in images:
+    img_path = img.get('src')
+    if not(homeUrl in img_path):
+        img_path = homeUrl+img_path
     try:
-        image_response = requests.get(f'{url}{img.get('src')}')
+        image_response = requests.get(f'{img_path}')
         if image_response.status_code != 200:
-            print('Error in image getting:', image_response.status_code)
+            print('Error in image getting:', f'{url}{img.get('src')}')
             sys.exit(0)
-        img_name = f'{storage_path}/{img.get('src').split('/')[-1]}'
+        img_name = f'{storage_path}/{img_path.split('/')[-1]}'
         with open(img_name, 'wb') as file:
             file.write(image_response.content)
         print('Successful download: ', img_name)
-
     except requests.exceptions.RequestException as e:
         print('Request error with img:', e)
 
